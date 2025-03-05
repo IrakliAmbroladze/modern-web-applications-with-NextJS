628f68b32fdb7dbf1c146458c75c40d4f0c94997
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-03-05 15:33:06 +0400

    use theme context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
+"use client";
+
+import { useContext, useState } from "react";
+import React from "react";
 import Welcome from "@/components/welcome";
 
+const ThemeContext = React.createContext();
+
 const HomePage = () => {
-  return <Welcome />;
+  const [theme, setTheme] = useState("light");
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <Welcome />
+      <Toolbar />
+    </ThemeContext.Provider>
+  );
 };
+
+function Toolbar() {
+  return <ThemeSwitch />;
+}
+
+function ThemeSwitch() {
+  const { theme, setTheme } = useContext(ThemeContext);
+  function toggleTheme() {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  }
+  return (
+    <button onClick={toggleTheme}>
+      Switch to {theme === "light" ? "dark" : "light"} theme
+    </button>
+  );
+}
 
 export default HomePage;
